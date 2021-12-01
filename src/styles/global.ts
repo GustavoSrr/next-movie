3ae822378489aaf52fd806458a98304573db20e5
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-01 18:35:06 -0300

    💄 Adicionando variáveis para cores

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,21 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const Global = createGlobalStyle`
+  :root {
+    --backgroundColor: #fff;
+    --darkerColor: #f0f0f0;
+    --titleColor: #333333;
+    --textColor: #444444;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+    background-color: var(---backgroundColor);
+    color: var(--textColor)
+  }
+
   * {
     margin: 0;
     padding: 0;
