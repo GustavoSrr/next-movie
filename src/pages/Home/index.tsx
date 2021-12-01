b82f6978c03c63018c0e52d52747f6c5f1e26faa
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-01 18:36:54 -0300

    ✨ Usando header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { Header } from '../../components/Header'
+import { useMovie } from '../../hooks/useMovie'
+
+import { Container } from './styles'
 
 export const Home: React.FC = () => {
+  const { movie } = useMovie()
+  useEffect(() => {
+    console.log(movie)
+  })
+
   return (
-    <h1>Home</h1>
+    <Container>
+      <Header />
+      <h1>Home</h1>
+    </Container>
   )
 }
