df236f16f7d4a241b287c043a90641e4db6b3e05
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-01 18:35:33 -0300

    ✨ Usando context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { MovieContextProvider } from './contexts/movie'
 
 import { AppRoutes } from './routes/routes'
 
 export const App: React.FC = () => {
   return (
-    <>
+    <MovieContextProvider>
       <AppRoutes />
-    </>
+    </MovieContextProvider>
   )
 }
