c948973d625e2b6beeb9c55324152b099640e414
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-02 16:05:45 -0300

    🔇 Removendo console.log

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import { Header } from '../../components/Header'
-import { useMovie } from '../../hooks/useMovie'
 
 import { Container } from './styles'
 
 export const Home: React.FC = () => {
-  const { movie } = useMovie()
-  useEffect(() => {
-    console.log(movie)
-  })
-
   return (
     <Container>
       <Header />
