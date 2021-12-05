fdacbb8163dbff39b5038230fe57ac4b15af0c6b
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-04 21:22:58 -0300

    ✨ Usando componente row para mostrar filmes e séries do momento

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,59 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useGet } from '../../hooks/useGet'
 
 import { Header } from '../../components/Header'
+import { Row } from '../../components/Row'
 
-import { Container } from './styles'
+import Collision from '../../assets/collision.svg'
+
+import type { TrendingType } from '../../contexts/get'
+
+import { Container, Content, Explore, Trending } from './styles'
 
 export const Home: React.FC = () => {
+  const { getTrending } = useGet()
+  const [trending, setTrending] = useState({} as TrendingType)
+
+  const getData = useCallback(async () => {
+    const { data } = await getTrending()
+
+    if (data) setTrending(data)
+  }, [])
+
+  useEffect(() => {
+    getData()
+  }, [0, getData])
+
   return (
-    <Container>
+    <>
       <Header />
-      <h1>Home</h1>
-    </Container>
+      <Container>
+        <Content>
+          <Explore>
+            <h1>Explorar</h1>
+            <form>
+              <input type="text" placeholder="Filmes e séries" />
+            </form>
+          </Explore>
+          <Trending>
+            {
+              trending
+                ? (
+                  <>
+                    <h2><img src={Collision} alt="💥" draggable="false" /> Bombando</h2>
+                    <Row
+                      page={trending.page}
+                      results={trending.results}
+                      total_pages={trending.total_pages}
+                      total_results={trending.total_results}
+                    />
+                  </>
+                  )
+                : ''
+            }
+          </Trending>
+        </Content>
+      </Container>
+    </>
   )
 }
