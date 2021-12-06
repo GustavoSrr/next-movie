7b5a1913438b7130de0363c42709efbcd0676711
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-06 16:55:23 -0300

    ♻️ Separando trending em um componente

---
+++ src/components/Trending/index.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { useGet } from '../../hooks/useGet'
+
+import { Row } from '../Row'
+
+import Collision from '../../assets/collision.svg'
+
+import type { TrendingType } from '../../contexts/get'
+
+import { Container } from './styles'
+
+export const Trending: React.FC = () => {
+  const { getTrending } = useGet()
+  const [trending, setTrending] = useState({} as TrendingType)
+
+  const getData = useCallback(async () => {
+    const { data } = await getTrending()
+
+    if (data) setTrending(data)
+  }, [])
+
+  useEffect(() => {
+    getData()
+  }, [0, getData])
+
+  return (
+    <Container>
+      {
+        trending
+          ? (
+            <>
+              <h2><img src={Collision} alt="💥" draggable="false" /> Bombando</h2>
+              <Row
+                page={trending.page}
+                results={trending.results}
+                total_pages={trending.total_pages}
+                total_results={trending.total_results}
+              />
+            </>
+            )
+          : ''
+      }
+    </Container>
+  )
+}
