9748dbcfde197848fe2893e1f5b0e3c5388b8e8d
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-06 18:32:18 -0300

    ♻️ Refatorando o funcionamento do componente

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -1,46 +1,53 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import type { TrendingType } from '../../contexts/get'
+import type { TrendingType, PopularMoviesType } from '../../contexts/get'
 
 import { Container, MovieCard, RatingDiv } from './styles'
 
-export const Row: React.FC<TrendingType> = ({ results }: TrendingType) => {
+type RowProps = {
+  data: TrendingType | PopularMoviesType;
+  type: 'trending' | 'movie' | 'tv';
+}
+
+export const Row: React.FC<RowProps> = ({ data, type }: RowProps) => {
   function hasVoteCount (voteCount: number, voteAverage: number) {
     if (voteCount === 0) return 'NA'
     return voteAverage
   }
 
+  const { results } = data
+
   return (
     <Container>
       {
       results
-        ? results.map((trending) => {
+        ? results.map((movie) => {
           return (
-            <MovieCard key={trending.id}>
+            <MovieCard key={movie.id}>
               {
-                trending.poster_path
+                movie.poster_path
                   ? <div>
-                      <Link to={`/${trending.media_type}/${trending.id}`}>
+                      <Link to={`/${movie.media_type ? movie.media_type : type}/${movie.id}`}>
                         <div id="PosterDiv">
                           <img
-                            src={'https://www.themoviedb.org/t/p/w220_and_h330_face' + trending.poster_path}
+                            src={'https://www.themoviedb.org/t/p/w220_and_h330_face' + movie.poster_path}
                             alt="Poster"
                           />
                         </div>
                       </Link>
                       <RatingDiv
                         id="RatingDiv"
-                        voteAverage={trending.vote_average}
-                        voteCount={trending.vote_count}
+                        voteAverage={movie.vote_average}
+                        voteCount={movie.vote_count}
                       >
-                      <h3>{hasVoteCount(trending.vote_count, trending.vote_average)}</h3>
+                      <h3>{hasVoteCount(movie.vote_count, movie.vote_average)}</h3>
                       </RatingDiv>
                     </div>
                   : ''
               }
-              <Link to={`/${trending.media_type}/${trending.id}`} style={{ textDecoration: 'none' }}>
-                <h3 id="MovieTitle">{trending.name || trending.title}</h3>
+              <Link to={`/${movie.media_type ? movie.media_type : type}/${movie.id}`} style={{ textDecoration: 'none' }}>
+                <h3 id="MovieTitle">{movie.name || movie.title}</h3>
               </Link>
             </MovieCard>
           )
