4ec9d8371809b02b65d1e6e442efdd6cec26124d
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-06 18:34:41 -0300

    ✨ Criando componente PopularMovies

---
+++ src/components/PopularMovies/index.tsx
@@ -0,0 +1,43 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { useGet } from '../../hooks/useGet'
+
+import { Row } from '../Row'
+
+import Popcorn from '../../assets/popcorn.svg'
+
+import type { PopularMoviesType } from '../../contexts/get'
+
+import { Container } from './styles'
+
+export const PopularMovies: React.FC = () => {
+  const { getPopularMovies } = useGet()
+  const [popularMovies, setPopularMovies] = useState({} as PopularMoviesType)
+
+  const getData = useCallback(async () => {
+    const { data } = await getPopularMovies()
+
+    if (data) setPopularMovies(data)
+  }, [])
+
+  useEffect(() => {
+    getData()
+  }, [0, getData])
+
+  return (
+    <Container>
+      {
+        popularMovies
+          ? (
+            <>
+              <h2><img src={Popcorn} alt="🍿" draggable="false" /> Filmes populares</h2>
+              <Row
+                data={popularMovies}
+                type="movie"
+              />
+            </>
+            )
+          : ''
+      }
+    </Container>
+  )
+}
