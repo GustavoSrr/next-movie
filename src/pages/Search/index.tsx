63f3ace30771c0a10fed28676a8357058411025d
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-09 18:09:18 -0300

    ✨ Criando página de pesquisa

---
+++ src/pages/Search/index.tsx
@@ -0,0 +1,75 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { useSearchParams, Link } from 'react-router-dom'
+import { useSearch } from '../../hooks/useSearch'
+
+import { Header } from '../../components/Header'
+
+import type { PopularType } from '../../contexts/get'
+
+import { Container, Content, MovieInfo, RatingDiv } from './styles'
+
+export const Search: React.FC = () => {
+  const [searchParams] = useSearchParams()
+  const q = searchParams.get('q')
+
+  const { search } = useSearch()
+  const [searchData, setSearchData] = useState({} as PopularType)
+
+  const getData = useCallback(async () => {
+    if (q) {
+      const { ok, data } = await search(q)
+
+      if (ok === false) console.log('error')
+
+      if (data) setSearchData(data)
+      console.log(data)
+    }
+  }, [0, q, searchParams])
+
+  useEffect(() => {
+    getData()
+  }, [0, q, searchParams])
+
+  const { results } = searchData
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <Content>
+          { results
+            ? results.filter(movie => movie.media_type !== 'person').map((movie) => {
+              return (
+                <Link
+                  to={`/${movie.media_type}/${movie.id}`}
+                  key={movie.id}
+                  id="MovieRectangle"
+                >
+                  <img
+                    src={movie.poster_path ? `https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}` : 'http://localhost:3000/src/assets/defaultPoster.jpg'}
+                    alt="Poster"
+                  />
+                  <MovieInfo>
+                    <h3 id="MovieTitle">{movie.title || movie.name}</h3>
+                    <RatingDiv
+                      voteCount={movie.vote_count}
+                      voteAverage={movie.vote_average}
+                    >
+                      <h3>{movie.vote_average}</h3>
+                    </RatingDiv>
+                    {
+                      movie.overview
+                        ? <p>{movie.overview}</p>
+                        : ''
+                    }
+                  </MovieInfo>
+                </Link>
+              )
+            })
+            : ''}
+
+        </Content>
+      </Container>
+    </>
+  )
+}
---
+++ src/pages/Search/styles.ts
@@ -0,0 +1,147 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  min-height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 80px;
+  margin-bottom: 80px;
+
+  background-color: var(--backgroundColor);
+`
+
+export const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 1220px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  gap: 40px;
+
+  #MovieRectangle {
+    width: 100%;
+
+    display: flex;
+    align-items: center;
+
+    gap: 35px;
+    padding: 20px;
+
+    text-decoration: none;
+    border: 1px solid transparent;
+    border-radius: 5px;
+    background-color: var(--complementaryColor);
+    transition: .2s;
+    cursor: pointer;
+
+    img {
+      max-width: 140px;
+
+      border-radius: 5px;
+    }
+
+    &:hover {
+      border-color: var(--primaryColor);
+      background-color: transparent;
+
+      #MovieTitle {
+        color: var(--titleColor);
+      }
+    }
+  }
+`
+
+export const MovieInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  gap: 20px;
+
+  #MovieTitle {
+    font-size: 18px;
+    font-weight: 500;
+    color: var(--textColor);
+    transition: .2s;
+    cursor: pointer;
+  }
+
+  p {
+    font-size: 15px;
+    color: var(--textColor);
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    max-width: 100%;
+  }
+`
+
+function ratingTransparentColor (voteAverage: number | string, voteCount: number) {
+  if (voteCount === 0) {
+    return 'var(--grayColorTransparent)'
+  }
+  if (voteAverage <= 5) {
+    return 'var(--redColorTransparent)'
+  }
+  if (voteAverage < 7) {
+    return 'var(--yellowColorTransparent)'
+  }
+  if (voteAverage >= 7) {
+    return 'var(--greenColorTransparent)'
+  }
+}
+
+function ratingColor (voteAverage: number | string, voteCount: number) {
+  if (voteCount === 0) {
+    return 'var(--grayColor)'
+  }
+  if (voteAverage <= 5) {
+    return 'var(--redColor)'
+  }
+  if (voteAverage < 7) {
+    return 'var(--yellowColor)'
+  }
+  if (voteAverage >= 7) {
+    return 'var(--greenColor)'
+  }
+}
+
+export const RatingDiv = styled.div<{ voteAverage: number, voteCount: number }>`
+  width: 40px;
+  height: 40px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 8px;
+
+  border-radius: 50%;
+  border: 1px solid ${props => ratingTransparentColor(props.voteAverage, props.voteCount)};
+  background-color: ${props => ratingTransparentColor(props.voteAverage, props.voteCount)};
+  color: ${props => ratingColor(props.voteAverage, props.voteCount)};
+  transition: .2s;
+
+  z-index: 1;
+
+  h3 {
+    font-size: 15px;
+  }
+
+  @media only screen and (max-width: 800px) {
+    width: 35px;
+    height: 35px;
+
+    h3 {
+      font-size: 13px;
+    }
+  }
+`
