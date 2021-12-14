ddf9e8b61fa477c4d97bc0f08bc7e27926d889ff
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-14 14:28:42 -0300

    🏷️ Adicionando tipagem NowPlaying

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import type { TrendingType, PopularType } from '../../contexts/get'
+import type { TrendingType, PopularType, NowPlayingType } from '../../contexts/get'
 
 import { Container, MovieCard, RatingDiv } from './styles'
 
 type RowProps = {
-  data: TrendingType | PopularType;
+  data: TrendingType | PopularType | NowPlayingType;
   type: 'trending' | 'movie' | 'tv';
 }
 
