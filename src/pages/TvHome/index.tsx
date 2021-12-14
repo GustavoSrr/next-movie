0c1627124663e8e2680949ba5a70ce568b58fb94
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-14 19:14:34 -0300

    ✨ Usando componente AiringToday

--- src/pages/TvHome/index.tsx
+++ src/pages/TvHome/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Header } from '../../components/Header'
 import { Popular } from '../../components/Popular'
 import { NowPlaying } from '../../components/NowPlaying'
+import { AiringToday } from '../../components/AiringToday'
 import { Footer } from '../../components/Footer'
 
 import { Container, Content } from './styles'
@@ -16,6 +17,7 @@ export const TvHome: React.FC = () => {
           <h1>Séries</h1>
           <Popular title="Populares" type="tv"/>
           <NowPlaying title="Em exibição" type="tv"/>
+          <AiringToday />
         </Content>
       </Container>
       <Footer />
