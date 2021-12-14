827b456d183ac7a53c6596bd2bc1ae914fae2993
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-14 14:31:51 -0300

    🌱 Criando rotas /movie e /tv

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { Home } from '../pages/Home'
+import { MovieHome } from '../pages/MovieHome'
+import { TvHome } from '../pages/TvHome'
 import { Movie } from '../pages/Movie'
 import { Tv } from '../pages/Tv'
 import { Search } from '../pages/Search'
@@ -14,6 +16,8 @@ export function AppRoutes () {
       <Global />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<MovieHome />} />
+        <Route path="/tv" element={<TvHome />} />
         <Route path="/movie/:id" element={<Movie />} />
         <Route path="/tv/:id" element={<Tv />} />
         <Route path="/search" element={<Search />} />
