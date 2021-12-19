55aaf6b3434ebdf6bbddbd8f9c86928f78459be2
Gustavo <gustavosrodrigues2000@gmail.com>
2021-12-18 21:31:23 -0300

    ♻️ Refatorando

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom'
 
 import Question from '../../assets/question.svg'
 
+import Cover from '../../assets/cover.jpg'
 import { Container, Content } from './styles'
 
 export const NotFound: React.FC = () => {
   return (
     <Container>
       <Content>
-        <img src={Question} alt="?" draggable="false" />
+        <img src={Cover} alt="?" draggable="false" />
         <div>
           <h1>Página não encontrada</h1>
           <Link id="HomeLink" to="/">Voltar para página principal</Link>
